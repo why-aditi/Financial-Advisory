fcec1413413ffca6b37e51892814a2fcab074dde
keshav1441 <keshavsharma1441@gmail.com>
2024-07-28 15:13:18 +0530

    marketing page reverse transition added

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -5,18 +5,18 @@ export default function Home() {
   const [isClicked, setIsClicked] = useState(false);
 
   const handleClick = () => {
-    setIsClicked(true);
+    setIsClicked(!isClicked);
   };
 
   return (
-    <div className={`parent-container ${isClicked ? "active" : ""}`}>
+    <div className={`parent-container ${isClicked ? "active" : "inactive"}`}>
       <div className={`bgimage ${isClicked ? "active" : ""}`}></div>
       <div className="logo2"></div>
       <div className="home">
         <div className="left-column">
           <div className="space"></div>
           <div className="space"></div>
-          <div className="financial space" onClick={handleClick}>
+          <div className={`financial space ${isClicked ? "active" : ""}`} onClick={handleClick}>
             Financial
           </div>
           <div className="space"></div>
@@ -25,27 +25,26 @@ export default function Home() {
         <div className="right-column">
           <div className="space"></div>
           <div className="space"></div>
-          <div className="advisory space" onClick={handleClick} >
+          <div className={`advisory space ${isClicked ? "active" : ""}`} onClick={handleClick}>
             Advisory
           </div>
           <div className="space"></div>
           <div className="space"></div>
-          
         </div>
-        <div className={`information-text ${isClicked ? "active" : ""}`}>
-                    <div className="col">
-                        <h2>A personal financial advisor for everyone</h2>
-                        <p>Personal financial advisors shouldn’t be limited to just a select group. We offer financial guidance to everyone, assisting with goal-setting, planning, budgeting, and achieving your financial objectives. </p>
-                    </div>
-                    <div className="col">
-                        <h2>Bridging gaps in wealth and financial literacy</h2>
-                        <p>Our mission is to make advanced, personalized financial guidance accessible to everyone and empowering them to navigate their financial journey with confidence.</p>
-                    </div>
-                    <div className="col">
-                        <h2>Onboard digitally and execute your trade orders with the click of a button.</h2>
-                        <p>Our platform makes trading simple and efficient, letting you manage your investments effortlessly.</p>
-                    </div>
-                </div>
+        <div className={`information-text ${isClicked ? "active" : "inactive"}`}>
+          <div className="col">
+            <h2>A personal financial advisor for everyone</h2>
+            <p>Personal financial advisors shouldn’t be limited to just a select group. We offer financial guidance to everyone, assisting with goal-setting, planning, budgeting, and achieving your financial objectives.</p>
+          </div>
+          <div className="col">
+            <h2>Bridging gaps in wealth and financial literacy</h2>
+            <p>Our mission is to make advanced, personalized financial guidance accessible to everyone and empowering them to navigate their financial journey with confidence.</p>
+          </div>
+          <div className="col">
+            <h2>Onboard digitally and execute your trade orders with the click of a button.</h2>
+            <p>Our platform makes trading simple and efficient, letting you manage your investments effortlessly.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
